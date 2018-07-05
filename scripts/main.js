8389097fc669b49f4d07bea716e99539167232f1
Tanxy Rogue <tanxyrogue@gmail.com>
2018-07-05 17:37:59 +0200

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,25 @@
+
 var heading = document.querySelector('h1')
-heading.innerHTML = "Tanxy's Sandbox"
+var p = document.querySelector('p')
+var body = document.querySelector('body')
+var footer = document.createElement('footer')
+var footerStatus = false
+
+footer.innerHTML = 'By C. Tanaka M.'
 heading.onclick = function () {
   if (heading.innerHTML === "Tanxy's Sandbox") {
     heading.innerHTML = 'Sandbox'
   } else {
     heading.innerHTML = "Tanxy's Sandbox"
+  }
+}
+
+p.onclick = function () {
+  if (footerStatus) {
+    body.removeChild(footer)
+    footerStatus = !footerStatus
+  } else {
+    body.appendChild(footer)
+    footerStatus = !footerStatus
   }
 }
